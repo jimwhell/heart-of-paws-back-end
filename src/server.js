0817aa52ad42f65523b058ea014abd0ee5c181c8
jimwell <jimwelllmanalo@gmail.com>
2025-03-26 01:41:06 +0800

    change origin to deployed frontend

--- src/server.js
+++ src/server.js
@@ -26,7 +26,7 @@ app.use(express.urlencoded({ extended: true }));
 // Configure CORS properly for cookies
 app.use(
   cors({
-    origin: "http://localhost:4200",
+    origin: "https://heartofpawstarlac.netlify.app/",
     credentials: true,
   })
 );
