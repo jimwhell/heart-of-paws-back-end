944de450d6f0b8660ccc9f5d808c3a509b7570bd
jimwell <jimwelllmanalo@gmail.com>
2025-03-25 13:59:43 +0800

    final commit

--- src/server.js
+++ src/server.js
@@ -56,7 +56,7 @@ app.use("/inquiries", inquiriesRouter);
 
 //error handler middleware
 app.use((err, req, res, next) => {
-  console.error("Global error handler:", err);
+  console.error(err);
 
   if (err instanceof CustomError) {
     return res.status(err.statusCode).json({
@@ -66,7 +66,6 @@ app.use((err, req, res, next) => {
 
   res.status(err.statusCode || 500).json({
     message: err.message || "Something went wrong, please try again later.",
-    error: process.env.NODE_ENV === "development" ? err : {},
   });
 });
 
